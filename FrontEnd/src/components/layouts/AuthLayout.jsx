57b730039a8aff33c8b3c1a7555f34a100c2851d
Vij4y18 <vijaysivakumar675@gmail.com>
2026-06-10 23:11:45 +0530

    Design & Build Login from UI

---
+++ FrontEnd/src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Card2 from "../../assets/images/Card2.png";
+import { LuTrendingUpDown } from "react-icons/lu";
+
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen">
+      {/* Left Section */}
+      <div className="w-full md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-lg font-medium text-black">
+          Expense Tracker
+        </h2>
+
+        <div className="flex items-center h-[calc(100vh-80px)]">
+          {children}
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="hidden md:block w-[40vw] h-screen bg-violet-50 overflow-hidden relative">
+        
+        {/* Decorative Shapes */}
+        <div className="w-48 h-48 rounded-[40px] bg-purple-700 absolute top-0 left-0"></div>
+
+        <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[35%] -right-16"></div>
+
+        <div className="w-48 h-48 rounded-[40px] bg-violet-500 absolute bottom-0 left-0"></div>
+
+        {/* Top Card */}
+        <div className="absolute top-8 left-8 right-8 z-20">
+          <StatsInfoCard
+            icon={<LuTrendingUpDown />}
+            label="Track your Income & Expenses"
+            value="430,000"
+            color="bg-purple-600"
+          />
+        </div>
+
+        {/* Chart Image */}
+        <img
+          src={Card2}
+          alt="Expense Tracker Card"
+          className="w-[90%] absolute bottom-10 left-8 z-10 drop-shadow-2xl"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
+
+const StatsInfoCard = ({ icon, label, value, color }) => {
+  return (
+    <div className="flex items-center gap-4 bg-white p-4 rounded-2xl shadow-lg">
+      <div
+        className={`w-12 h-12 flex items-center justify-center text-[24px] text-white ${color} rounded-full`}
+      >
+        {icon}
+      </div>
+
+      <div>
+        <h6 className="text-sm text-gray-500">
+          {label}
+        </h6>
+
+        <span className="text-3xl font-semibold text-gray-900">
+          ${value}
+        </span>
+      </div>
+    </div>
+  );
+};
--- FrontEnd/src/pages/Auth/Login.jsx
+++ FrontEnd/src/pages/Auth/Login.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import AuthLayout from '../../components/layouts/AuthLayout'
+
 const login = () => {
   return (
-    <div>login</div>
+    <AuthLayout>
+      <div className='lg:w[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back !!</h3>
+        <p className='text-xs text-slate-700 mt-1.25 mb-6'>
+          Please enter you details to login
+        </p>
+      </div>
+    </AuthLayout>
   )
 }
 
